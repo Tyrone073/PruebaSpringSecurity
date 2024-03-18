70db6782690849da2833dd006d3f92a009a9062f
Tyrone Pilay <tyronepilay11@gmail.com>
2024-03-18 10:16:00 -0500

    initial commit

---
+++ pruebaSpringSecurity/src/main/java/com/tyrone/pruebaSpringSecurity/PruebaSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.tyrone.pruebaSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ pruebaSpringSecurity/src/test/java/com/tyrone/pruebaSpringSecurity/PruebaSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyrone.pruebaSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
